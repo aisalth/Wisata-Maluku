5b55d8356c19c943b55b3b33064ebd4e6466c6df
aisalth <aisyah.altho38@smk.belajar.id>
2026-01-30 09:20:44 +0700

    ini baru

--- index.php
+++ index.php
@@ -44,7 +44,7 @@ include "/opt/lampp/htdocs/app_wisata/koneksi.php";
         </div>
     </div>
 
-    <h2>Ini contoh aja</h2>
+    <h2>Ini tambahan baru</h2>
 
     <div class="stats-section">
         <div class="stats">
